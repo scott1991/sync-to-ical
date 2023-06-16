b50b48d91cefe92fdb4c0b98e91833aedcabf8ea
scott1991 <cakepbm@gmail.com>
2023-06-17 01:19:25 +0800

    Add error handling

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ getIcs().then(data => {
   icsData = data;
   console.log('ICS updated', icsData.fetchDate.toISOString());
   saveData(icsData.content, icsData.fetchDate);
-});
+}).catch(err => {
+  console.error(err);
+  process.exit(1);
+})
 
 // serve icsdata as calendar.ics
 http.createServer((req, res) => {
@@ -25,8 +28,13 @@ console.log('Server running');
 
 // update icsData schedulled by cron
 cron.schedule(config.cronSchedule, async () => {
-  icsData = await getIcs();
-  console.log('ICS updated', icsData.fetchDate.toISOString());
-  saveData(icsData.content, icsData.fetchDate);
+  try {
+    icsData = await getIcs();
+    console.log('ICS updated', icsData.fetchDate.toISOString());
+    saveData(icsData.content, icsData.fetchDate); 
+  } catch (error) {
+    console.error('Fetch error');
+    console.error(error);
+  }
 });
 console.log('Cron scheduled');
